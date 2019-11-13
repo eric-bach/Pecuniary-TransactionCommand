4c258a6c97da1ae3b08e028bf4ba0f3b6415aaad
Eric Bach <bach.eric@gmail.com>
2019-11-12 21:35:16 -0700

    Split classes

--- Pecuniary.Transaction.Data/Requests/CreateTransactionRequest.cs
+++ Pecuniary.Transaction.Data/Requests/CreateTransactionRequest.cs
@@ -11,14 +11,3 @@ namespace Pecuniary.Transaction.Data.Requests
         public decimal Commission { get; set; }
     }
 }
-
-namespace Pecuniary.Transaction.Data.Requests
-{
-    public class CreateSecurityRequest
-    {
-        public Guid SecurityId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string ExchangeTypeCode { get; set; }
-    }
-}
